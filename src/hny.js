a683e13ff4de5ffe3be952757652c741f304fc89
Jessica Kerr <jessitron@gmail.com>
2025-04-13 17:41:07 -0500

    bug fix

--- src/hny.js
+++ src/hny.js
@@ -215,12 +215,7 @@ function addSpanEvent(message, attributes) {
 }
 
 function inChildSpan(inputTracer, spanName, spanContext, fn) {
-  if (
-    !!spanContext &&
-    (!spanContext.spanId ||
-      !spanContext.traceId ||
-      spanContext.traceFlags === undefined)
-  ) {
+  if (!!spanContext || !spanContext.spanId || !spanContext.traceId) {
     console.log(
       "inChildSpan: the third argument should be a spanContext (or undefined to use the active context)"
     );
